ba80b4c40c8d0e2dd778fa322188ad1fa9d68c6a
Simon Engelbrecht Sørensen <s.engelbrecht@gmail.com>
2022-01-22 18:17:02 +0100

    Shaderstuff

--- Assets/Shader/NoiseCustom.hlsl
+++ Assets/Shader/NoiseCustom.hlsl
@@ -5,6 +5,7 @@ void SimplexNoise3D_float (float3 uv, float scale, out float noise)
 {
 	noise = SimplexNoise(uv / scale);
 }
+
 void ClassicNoise3D_float (float3 uv, float scale, out float noise)
 {
 	noise = ClassicNoise(uv / scale);
